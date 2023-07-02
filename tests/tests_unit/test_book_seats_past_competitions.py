2d34f0fe65c72c950a06002e82fe988be6bfee9a
munchou <valounhan@gmail.com>
2023-07-02 20:52:01 +0900

    Update + conftest.py

--- tests/tests_unit/test_book_seats_past_competitions.py
+++ tests/tests_unit/test_book_seats_past_competitions.py
@@ -1,32 +1,28 @@
-from server import app, check_competition_date
+import server
+from server import check_competition_date
 
 
 class TestSeatsPastCompetition:
-    competitions_test = [
-        {
-            "name": "Spring Festival",
-            "date": "2028-03-27 10:00:00",
-            "numberOfPlaces": "25",
-        },
-        {
-            "name": "Fall Classic",
-            "date": "2020-10-22 13:30:00",
-            "numberOfPlaces": "13",
-        },
-    ]
-
     def test_past_competition(self):
-        competition = self.competitions_test[1]
-        competitions_over, competitions_ongoing = check_competition_date(
-            self.competitions_test
-        )
+        """
+        GIVEN an existing competition
+        WHEN the competition is being loaded
+        THEN check the competition date is prior
+        to the current date.
+        """
+        competitions = server.competitions
+        competitions_over, competitions_ongoing = check_competition_date(competitions)
 
-        assert competition in competitions_over
+        assert competitions_over == [competitions[1]]
 
     def test_coming_competition(self):
-        competition = self.competitions_test[0]
-        competitions_over, competitions_ongoing = check_competition_date(
-            self.competitions_test
-        )
+        """
+        GIVEN an existing competition
+        WHEN the competition is being loaded
+        THEN check the current date is prior
+        to the competition date.
+        """
+        competitions = server.competitions
+        competitions_over, competitions_ongoing = check_competition_date(competitions)
 
-        assert competition in competitions_ongoing
+        assert competitions_ongoing == [competitions[0]]
