462048653b96d1ffc57bb39ed97f9f902de589f7
munchou <valounhan@gmail.com>
2023-07-02 20:50:58 +0900

    Update + conftest.py

--- tests/tests_unit/test_up_to_12_seats.py
+++ tests/tests_unit/test_up_to_12_seats.py
@@ -1,15 +1,25 @@
-from server import app, clubs, competitions, club_booked_seats
+import server
+from server import app
 
 
 class TestUpToTwelveSeats:
     client = app.test_client()
 
-    def setup_method(self):
-        self.club = clubs[0]
-
     def test_purchase_more_than_twelve_seats(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club tries to book more than 12 seats
+        THEN check the club's points and the
+        competition's points have not changed and a
+        warning message was returned.
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        # all_booked_seats = server.all_booked_seats
+        club = clubs[0]
         competition = competitions[0]
+        points_before_booking = int(club["points"])
+        seats_before_booking = int(competition["numberOfPlaces"])
         response = self.client.post(
             "/purchasePlaces",
             data={
@@ -20,38 +30,65 @@ class TestUpToTwelveSeats:
         )
 
         assert response.status_code == 200
-        assert int(club["points"]) >= 0
+        assert int(club["points"]) == points_before_booking
+        assert int(competition["numberOfPlaces"]) == seats_before_booking
         assert "You cannot book more than 12 seats at once." in response.data.decode()
 
     def test_purchase_up_to_twelve_seats(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books 12 seats (= max allowed)
+        THEN check the club's points and the
+        competition's points have been updated and a
+        success message was returned.
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        # all_booked_seats = server.all_booked_seats
+        club = clubs[0]
         competition = competitions[0]
+        points_before_booking = int(club["points"])
+        seats_before_booking = int(competition["numberOfPlaces"])
+        booked_seats = 12
         response = self.client.post(
             "/purchasePlaces",
             data={
                 "club": club["name"],
                 "competition": competition["name"],
-                "places": 12,
+                "places": booked_seats,
             },
         )
+        # print(f"\nBOOKED SEATS: {booked_seats}\n")
         assert response.status_code == 200
-        assert int(club["points"]) >= 0
+        assert int(club["points"]) == points_before_booking - booked_seats
+        assert competition["numberOfPlaces"] == seats_before_booking - booked_seats
         assert "Great! Booking complete!" in response.data.decode()
 
     def test_purchase_a_13th_seat(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books 12 seats (= max allowed)
+        THEN check the club's points and the
+        competition's points have been updated and a
+        success message was returned.
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        all_booked_seats = server.all_booked_seats
+        club = clubs[0]
         competition = competitions[0]
+        all_booked_seats[0]["booked_seats"] = 12
+        booked_seats = 1
         response = self.client.post(
             "/purchasePlaces",
             data={
                 "club": club["name"],
                 "competition": competition["name"],
-                "places": 1,
+                "places": booked_seats,
             },
         )
 
         assert response.status_code == 200
-        assert int(club["points"]) >= 0
         assert (
             "You cannot book more than 12 seats for each competition."
             in response.data.decode()
