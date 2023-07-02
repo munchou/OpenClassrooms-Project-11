98114f6446806392160cfb7275d467b366cc358a
munchou <valounhan@gmail.com>
2023-07-02 20:49:54 +0900

    Update + conftest.py

--- tests/tests_unit/test_allowed_points.py
+++ tests/tests_unit/test_allowed_points.py
@@ -1,30 +1,22 @@
-from server import app, clubs, competitions
+import server
+from server import app
 
 
 class TestAllowedPoints:
     client = app.test_client()
 
-    club_test = [
-        {
-            "name": "Club",
-            "email": "test@club.com",
-            "points": "13",
-        }
-    ]
-    competition_test = [
-        {
-            "name": "Competition",
-            "date": "2020-03-27 10:00:00",
-            "numberOfPlaces": "25",
-        }
-    ]
-
-    def setup_method(self):
-        self.club = clubs[1]
-
     def test_purchase_with_enough_point(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books seats with enough points (<= 12)
+        THEN check the booking was successful and a
+        confirmation message was displayed
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        club = clubs[0]
         competition = competitions[0]
+
         response = self.client.post(
             "/purchasePlaces",
             data={
@@ -39,7 +31,15 @@ class TestAllowedPoints:
         assert "Great! Booking complete!" in response.data.decode()
 
     def test_purchase_not_enough_points(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books seats with not enough points
+        THEN check the booking was cancelled and a
+        warning was displayed
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        club = clubs[1]
         competition = competitions[0]
         response = self.client.post(
             "/purchasePlaces",
@@ -57,7 +57,18 @@ class TestAllowedPoints:
         )
 
     def test_purchase_OK_with_negative_input(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books seats with enough points (<= 12)
+        but entered a minus before the points (ex: "-5")
+        THEN check the booking was successful and a
+        confirmation message was displayed, which means
+        the negative input was converted its a positive
+        counterpart
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        club = clubs[0]
         competition = competitions[0]
         response = self.client.post(
             "/purchasePlaces",
@@ -73,7 +84,15 @@ class TestAllowedPoints:
         assert "Great! Booking complete!" in response.data.decode()
 
     def test_purchase_with_null_input(self):
-        club = self.club
+        """
+        GIVEN an existing club and competition
+        WHEN the club books 0 seat
+        THEN check the booking was cancelled and a
+        warning was displayed
+        """
+        clubs = server.clubs
+        competitions = server.competitions
+        club = clubs[0]
         competition = competitions[0]
         response = self.client.post(
             "/purchasePlaces",
